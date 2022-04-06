232f991735ee62c9425d9b7c1a6d04d9ddd112b3
Kirill <Rudovichkirill@gmail.com>
2022-04-06 15:11:32 +0300

    fix

--- glued_files_in_cpp/cpm_compilater.cpp
+++ glued_files_in_cpp/cpm_compilater.cpp
@@ -1415,7 +1415,7 @@ std::string AST::generate_cpp_command()
 int main(int argc, char* argv[]) {
 	try {
 		std::string file_path = argv[1];
-
+	
 		lexer Lexer(file_path);
 		Lexer.generate_tokens();
 
@@ -1432,21 +1432,17 @@ int main(int argc, char* argv[]) {
         file_path.resize(file_path.size() - 1);
 
         std::string file_name = argv[0];
-		while (file_name[file_name.size() - 1] != '\\')
+		while (file_name.size() > 0 && file_name[file_name.size() - 1] != '\\')
 		{
 			file_name.resize(file_name.size() - 1);
 		}
         std::string name;
-        while (file_path[file_path.size() - 1] != '\\')
+        while (file_path.size() > 0 && file_path[file_path.size() - 1] != '\\')
 		{
 			name = file_path[file_path.size() - 1] + name;
             file_path.resize(file_path.size() - 1);
 		}
         file_name += name;
-		char _name_[file_name.size()];
-		for (int i = 0;i<file_name.size();++i){
-			_name_[i] = file_name[i];
-		}
 		cpp_generator Cpp_generator(Syntaxer.get_commands(), file_name);
 		Cpp_generator.generate_cpp_code();
 		system(("g++ -o " + file_path + name + " " + file_name + ".cpp & " + file_path + name +".exe").c_str());
